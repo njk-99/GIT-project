0174ab91578d2591c1a24d00cef782e1765e78fd
njk-99 <128781925+njk-99@users.noreply.github.com>
2023-04-01 23:33:14 +0530

    Add files via upload

---
+++ PositiveNigative.java
@@ -0,0 +1,16 @@
+package que2;
+import java.util.*;
+public class PositiveNigative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		System.out.println("enert the number");
+		int n=s .nextInt();
+		String Number= n>=0? "Positive Number":"Negative Number";
+		System.out.println(Number);
+		
+
+	}
+
+}
